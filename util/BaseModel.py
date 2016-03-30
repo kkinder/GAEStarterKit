e07b2e24cddc8a2e2d1c8838e8509b2009344714
Ken Kinder <ken@kkinder.com>
2016-03-30 16:26:05 -0300

    Add a utility method to get instances from urlsafe key.

--- util/BaseModel.py
+++ util/BaseModel.py
@@ -7,3 +7,10 @@ from google.appengine.ext import ndb
 class BaseModel(ndb.Model):
     date_created = ndb.DateTimeProperty(auto_now_add=True, required=True)
     date_modified = ndb.DateTimeProperty(auto_now=True, required=True)
+
+    @classmethod
+    def from_urlsafe(cls, urlsafe):
+        key = ndb.Key(urlsafe=urlsafe)
+        obj = key.get()
+        if obj and isinstance(obj, cls):
+            return obj
