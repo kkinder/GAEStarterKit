63e422617eff6bde750b0b3e2d0e3eea89aeb19d
Ken Kinder <ken@kkinder.com>
2016-04-19 19:31:58 -0300

    Throw in Google's ndb counter example.

--- util/counters.py
+++ util/counters.py
@@ -1,13 +1,26 @@
-"""
-Based on https://github.com/GoogleCloudPlatform/appengine-sharded-counters-python
+# Copyright 2008 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-Sharded counters for data
-"""
+"""A module implementing a general sharded counter."""
+
 
 import random
 
 from google.appengine.api import memcache
 from google.appengine.ext import ndb
+
 
 SHARD_KEY_TEMPLATE = 'shard-{}-{:d}'
 
