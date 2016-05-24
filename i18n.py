ed1821a4e37f6e57142b0bd9ee861b68c748567f
Ken Kinder <ken@kkinder.com>
2016-05-24 10:19:04 -0400

    Add translation-generating code to i18n.py

--- i18n.py
+++ i18n.py
@@ -1,6 +1,23 @@
 """
-Dealing with internationalization
+This module contains code dealing with internationalization.
+
+When imported, it adds the babel configuration to the existing flask app.
+
+When called directly, it updates the "pot" file
 """
+if __name__ == '__main__':
+    import subprocess, sys, os
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(path)
+    # noinspection Restricted_Python_calls
+    subprocess.check_call('pybabel extract -F babel.cfg -k lazy_gettext -o messages.pot .', shell=True)
+    # noinspection Restricted_Python_calls
+    subprocess.check_call('pybabel update -i messages.pot -d translations', shell=True)
+    # noinspection Restricted_Python_calls
+    subprocess.check_call('pybabel compile -d translations', shell=True)
+    sys.exit(0)
+
 import datetime
 
 from flask.ext.babel import Babel
@@ -13,11 +30,15 @@ import config
 ##
 ## Internationalization
 babel = Babel(app)
+
+
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(config.languages.keys())
 
+
 moment = Moment(app)
+
 
 @app.context_processor
 def inject_locale():
@@ -28,4 +49,3 @@ def inject_locale():
         js_timeformat='HH:MM:SS',
         js_datetimeformat='YYYY-MM-DD HH:MM:SS'
     )
-
